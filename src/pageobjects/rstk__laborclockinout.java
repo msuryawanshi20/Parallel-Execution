cfc66ba0d8cf29c57ccb5ab33f1c1d33d3063e82
Mayur Suryawanshi <msuryawanshi@rootstock.com>
2022-06-02 15:49:38 +0530

    Added Wait

--- src/pageobjects/rstk__laborclockinout.java
+++ src/pageobjects/rstk__laborclockinout.java
@@ -42,6 +42,7 @@ public class rstk__laborclockinout {
 	@FindBy(xpath = "//label[normalize-space(.)='Show Only Operations with Qty Available']/parent::span/parent::th/following-sibling::td//input")
 	public WebElement showOnlyOperationswithQtyAvailable;
 
+	@PageWaitAfter.Field(field = "list", timeoutSeconds = 10)
 	@TextType()
 	@FindBy(xpath = "//label[normalize-space(.)='Search Work Orders']/parent::span/parent::th/following-sibling::td//input")
 	public WebElement searchWorkOrders;
@@ -50,6 +51,7 @@ public class rstk__laborclockinout {
 	@FindBy(xpath = "//li[@id='li-0']")
 	public WebElement list;
 
+	@PageWaitAfter.Field(field = "timeAndQtyBookingDetailsTable", timeoutSeconds = 10)
 	@ButtonType()
 	@FindByLabel(label = "Load Entry Grid")
 	public WebElement loadEntryGrid;
@@ -57,33 +59,47 @@ public class rstk__laborclockinout {
 	@PageRow()
 	public static class TimeAndQtyBookingDetailsTable {
 
+		@PageWait.Field(timeoutSeconds = 10)
 		@ChoiceListType()
 		@FindBy(xpath = ".//tr/td[2]/select")
 		public WebElement bookingType;
 
+		@PageWait.Field(timeoutSeconds = 10)
 		@TextType()
 		@FindBy(xpath = ".//tr/td[3]//input")
 		public WebElement transactionDate;
 
+
+		@PageWait.Field(timeoutSeconds = 10)
 		@TextType()
 		@FindBy(xpath = ".//tr/td[6]//input")
 		public WebElement hours;
 
+
+		@PageWait.Field(timeoutSeconds = 10)
 		@ChoiceListType()
 		@FindBy(xpath = ".//tr/td[7]//select")
 		public WebElement workOrder;
 
+
+		@PageWait.Field(timeoutSeconds = 10)
 		@ChoiceListType()
 		@FindBy(xpath = ".//tr/td[8]//select")
 		public WebElement operation;
 
+
+		@PageWait.Field(timeoutSeconds = 10)
 		@BooleanType()
 		@FindBy(xpath = ".//tr/td[9]//input")
 		public WebElement setupLabor;
 
+
+
 		@BooleanType()
 		@FindBy(xpath = ".//tr/td[10]//input")
 		public WebElement complete;
+
+
 
 		@BooleanType()
 		@FindBy(xpath = ".//tr/td[11]//input")
