ed022848a381a595039443354f9e307c115efdcc
Mayur Suryawanshi <msuryawanshi@rootstock.com>
2022-06-06 17:49:05 +0530

    SOCONPP changes

--- src/pageobjects/rstk__Soconpp.java
+++ src/pageobjects/rstk__Soconpp.java
@@ -1,14 +1,8 @@
 package pageobjects;
 
-import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.provar.core.testapi.annotations.ButtonType;
 import com.provar.core.testapi.annotations.ChoiceListType;
